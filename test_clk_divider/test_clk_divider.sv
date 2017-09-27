3ceff8cdb6bd111cbde803a50f9951fbde4e53ee
zachmorris <zachmorris23@gmail.com>
2017-09-26 20:23:16 -0700

    Added asynch reset to the clock divider
    Triggers on negedge of reset signal, eg switch

--- test_clk_divider/test_clk_divider.sv
+++ test_clk_divider/test_clk_divider.sv
@@ -8,10 +8,10 @@ module test_clk_divider(inclk,outclk,outclk_Not,div_clk_count,Reset, count_check
 	logic [31:0] count;  				// count up to div_clk_count
 	output [31:0] count_check;
 	
-	always_ff @(posedge inclk, posedge Reset)
+	always_ff @(posedge inclk, negedge Reset)
 	begin	 
 		count_check <= count % div_clk_count;
-		if (Reset) 
+		if (Reset == 1'b0) 
 			begin  
 				outclk <= 0;  		// default value for outclk
 				outclk_Not <= 1;	// default value for outclk_Not
